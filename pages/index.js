dbcda4edcbe3dc66dac0c9d4a631f58ce481cdda
Lee-sangjae-jpg <omygrocery@gmail.com>
2025-12-10 09:11:43 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>싼재네 오프라인 주문 웹앱입니다</h1>
+      <p>이 페이지 위에 날짜, 상품, 주문 기능을 하나씩 올릴 거예요.</p>
+    </div>
+  );
+}
